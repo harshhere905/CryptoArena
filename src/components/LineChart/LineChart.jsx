f1915c53dd819f478f9079284c7530d803f9727d
Harshhere905 <hv96829@gmail.com>
2024-12-29 00:19:52 +0530

    Updated LineChart to format dates as DD/MM

--- src/components/LineChart/LineChart.jsx
+++ src/components/LineChart/LineChart.jsx
@@ -1,24 +1,29 @@
-import React, { useEffect, useState } from 'react'
-import Chart from 'react-google-charts'
-const LineChart = ({historicalData}) => {
-   const [data,setData]=useState([["Date","Prices"]])
-   useEffect(()=>{
-      let dataCopy = [["Data","Prices"]];
-      if(historicalData.prices){
-        historicalData.prices.map((item)=>{
-           dataCopy.push([`${new Date(item[0]).toLocaleDateString().slice(0.-5)}`,item[1]])
-        })
-        setData(dataCopy);
-      }
-   },[historicalData])
-    return (
-     <Chart
-          chartType='LineChart'
-          data={data}
-          height="100%"
-          legendToggle
-     />
-  )
-}
+import React, { useEffect, useState } from 'react';
+import Chart from 'react-google-charts';
 
-export default LineChart
+const LineChart = ({ historicalData }) => {
+  const [data, setData] = useState([["Date", "Prices"]]);
+
+  useEffect(() => {
+    let dataCopy = [["Date", "Prices"]];
+    if (historicalData.prices) {
+      historicalData.prices.forEach((item) => {
+        const date = new Date(item[0]);
+        const formattedDate = `${String(date.getDate()).padStart(2, '0')}/${String(date.getMonth() + 1).padStart(2, '0')}`; // Format to "DD/MM"
+        dataCopy.push([formattedDate, item[1]]);
+      });
+      setData(dataCopy);
+    }
+  }, [historicalData]);
+
+  return (
+    <Chart
+      chartType='LineChart'
+      data={data}
+      height="100%"
+      legendToggle
+    />
+  );
+};
+
+export default LineChart;
